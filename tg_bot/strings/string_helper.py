f7faddb08cda6782cb8adb0d4587eca4c1f43c70
Jens <jens@karatek.net>
2020-07-29 18:07:10 +0200

    Added get_random_string to string_helper.py for /runs and /slap

--- tg_bot/strings/string_helper.py
+++ tg_bot/strings/string_helper.py
@@ -1,9 +1,18 @@
 import json
 import os
+import random
 
 
+# get string from json file
 def get_string(module: str, name: str, lang: str):
     with open(os.path.dirname(os.path.abspath(__file__)) + "/" + lang + "/" + module + ".json") as f:
         data = json.load(f)
 
     return data[name]
+
+# for /runs, /slap etc.
+def get_random_string(module: str, lang: str):
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/" + lang + "/" + module + ".json") as f:
+        data = json.load(f)
+        i = len(data)
+        return data[random.randint(1, i)]
