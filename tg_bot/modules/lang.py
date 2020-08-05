844660c10824ef0bb03f030eb759bae7b01455bd
Jens <jens@karatek.net>
2020-08-05 14:16:51 +0200

    Experimented with lang.py

---
+++ tg_bot/modules/lang.py
@@ -0,0 +1,23 @@
+from telegram import Bot
+from telegram.ext import run_async, CommandHandler
+
+from tg_bot import dispatcher
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin
+
+
+@run_async
+@bot_admin
+@user_admin
+def setlang(bot: Bot, update: Update, args: List[str]):
+    chat_id = update.effective_chat.id
+    message = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+    message.reply_message("Test")
+
+__mod_name__ = "Languages"
+
+__help__ = "Test\n\nThis is a test."
+
+LANG_HANDLER = CommandHandler("lang", setlang, pass_args=True)
+
+dispatcher.add_handler(LANG_HANDLER)
