731d7ccf0f4c2c9d3f37c9347a2821b9c544e5f7
Jens <jens@karatek.net>
2020-10-21 12:21:48 +0200

    fix a bug with /globalmute which broke other commands

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -34,6 +34,7 @@ from tg_bot.strings.string_helper import get_string
 import tg_bot.modules.sql.lang_sql as lang
 import tg_bot.modules.sql.mute as mute_sql
 
+
 @run_async
 @bot_admin
 @user_admin
@@ -51,12 +52,13 @@ def toggle_mute(update: Update, context: CallbackContext) -> str:
         msg.reply_text("This chat is now muted, only administrators may write now!")
         return f"{update.effective_user.first_name} muted the chat!"
 
+
 @run_async
 @user_not_admin
 def on_message(update: Update, context: CallbackContext):
+    LOGGER.debug("Yeet!")
     if mute_sql.get_muted(update.effective_chat.id):
-        message = update.effective_message  # type: Optional[Message]
-        message.delete()
+        update.effective_message.delete()
 
 
 @run_async
@@ -75,16 +77,18 @@ def promote(update: Update, context: CallbackContext) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text(get_string("admin", "ERR_NO_USER", lang.get_lang(update.effective_chat.id))) # ERR_NO_USER
+        message.reply_text(get_string("admin", "ERR_NO_USER", lang.get_lang(update.effective_chat.id)))  # ERR_NO_USER
         return ""
 
     user_member = chat.get_member(user_id)
     if user_member.status == 'administrator' or user_member.status == 'creator':
-        message.reply_text(get_string("admin", "ERR_CANT_PROMOTE_ADMIN", lang.get_lang(update.effective_chat.id))) # ERR_CANT_PROMOTE_ADMIN
+        message.reply_text(get_string("admin", "ERR_CANT_PROMOTE_ADMIN",
+                                      lang.get_lang(update.effective_chat.id)))  # ERR_CANT_PROMOTE_ADMIN
         return ""
 
     if user_id == bot.id:
-        message.reply_text(get_string("admin", "ERR_CANT_PROMOTE_MYSELF", lang.get_lang(update.effective_chat.id))) # ERR_CANT_PROMOTE_MYSELF
+        message.reply_text(get_string("admin", "ERR_CANT_PROMOTE_MYSELF",
+                                      lang.get_lang(update.effective_chat.id)))  # ERR_CANT_PROMOTE_MYSELF
         return ""
 
     # set same perms as bot - bot can't assign higher perms than itself!
@@ -100,10 +104,12 @@ def promote(update: Update, context: CallbackContext) -> str:
                           can_pin_messages=bot_member.can_pin_messages,
                           can_promote_members=bot_member.can_promote_members)
 
-    message.reply_text(get_string("admin", "PROMOTE_SUCCESS", lang.get_lang(update.effective_chat.id))) # PROMOTE_SUCCESS
-    return get_string("admin", "PROMOTE_SUCCESS_HTML", lang.get_lang(update.effective_chat.id)).format(html.escape(chat.title),
-                                      mention_html(user.id, user.first_name),
-                                      mention_html(user_member.user.id, user_member.user.first_name)) # PROMOTE_SUCCESS_HTML
+    message.reply_text(
+        get_string("admin", "PROMOTE_SUCCESS", lang.get_lang(update.effective_chat.id)))  # PROMOTE_SUCCESS
+    return get_string("admin", "PROMOTE_SUCCESS_HTML", lang.get_lang(update.effective_chat.id)).format(
+        html.escape(chat.title),
+        mention_html(user.id, user.first_name),
+        mention_html(user_member.user.id, user_member.user.first_name))  # PROMOTE_SUCCESS_HTML
 
 
 @run_async
@@ -121,20 +127,23 @@ def demote(update: Update, context: CallbackContext) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text(get_string("admin", "ERR_NO_USER", lang.get_lang(update.effective_chat.id))) # ERR_NO_USER
+        message.reply_text(get_string("admin", "ERR_NO_USER", lang.get_lang(update.effective_chat.id)))  # ERR_NO_USER
         return ""
 
     user_member = chat.get_member(user_id)
     if user_member.status == 'creator':
-        message.reply_text(get_string("admin", "ERR_DEMOTE_CREATOR", lang.get_lang(update.effective_chat.id))) # ERR_DEMOTE_CREATOR
+        message.reply_text(
+            get_string("admin", "ERR_DEMOTE_CREATOR", lang.get_lang(update.effective_chat.id)))  # ERR_DEMOTE_CREATOR
         return ""
 
     if not user_member.status == 'administrator':
-        message.reply_text(get_string("admin", "ERR_DEMOTE_NON_ADMIN", lang.get_lang(update.effective_chat.id))) # ERR_DEMOTE_NON_ADMIN
+        message.reply_text(get_string("admin", "ERR_DEMOTE_NON_ADMIN",
+                                      lang.get_lang(update.effective_chat.id)))  # ERR_DEMOTE_NON_ADMIN
         return ""
 
     if user_id == bot.id:
-        message.reply_text(get_string("admin", "ERR_CANT_DEMOTE_MYSELF", lang.get_lang(update.effective_chat.id))) # ERR_CANT_DEMOTE_MYSELF
+        message.reply_text(get_string("admin", "ERR_CANT_DEMOTE_MYSELF",
+                                      lang.get_lang(update.effective_chat.id)))  # ERR_CANT_DEMOTE_MYSELF
         return ""
 
     try:
@@ -147,13 +156,15 @@ def demote(update: Update, context: CallbackContext) -> str:
                               can_restrict_members=False,
                               can_pin_messages=False,
                               can_promote_members=False)
-        message.reply_text(get_string("admin", "DEMOTE_SUCCESS", lang.get_lang(update.effective_chat.id))) # DEMOTE_SUCCESS
-        return get_string("admin", "DEMOTE_SUCCESS_HTML", lang.get_lang(update.effective_chat.id)).format(html.escape(chat.title),
-                                          mention_html(user.id, user.first_name),
-                                          mention_html(user_member.user.id, user_member.user.first_name)) # DEMOTE_SUCCESS_HTML
+        message.reply_text(
+            get_string("admin", "DEMOTE_SUCCESS", lang.get_lang(update.effective_chat.id)))  # DEMOTE_SUCCESS
+        return get_string("admin", "DEMOTE_SUCCESS_HTML", lang.get_lang(update.effective_chat.id)).format(
+            html.escape(chat.title),
+            mention_html(user.id, user.first_name),
+            mention_html(user_member.user.id, user_member.user.first_name))  # DEMOTE_SUCCESS_HTML
 
     except BadRequest:
-        message.reply_text(get_string("admin", "ERR_GENERAL", lang.get_lang(update.effective_chat.id))) # ERR_GENERAL
+        message.reply_text(get_string("admin", "ERR_GENERAL", lang.get_lang(update.effective_chat.id)))  # ERR_GENERAL
         return ""
 
 
@@ -184,7 +195,8 @@ def pin(update: Update, context: CallbackContext) -> str:
                 pass
             else:
                 raise
-        return get_string("admin", "PINNED_HTML", lang.get_lang(update.effective_chat.id)).format(html.escape(chat.title), mention_html(user.id, user.first_name)) # PINNED_HTML
+        return get_string("admin", "PINNED_HTML", lang.get_lang(update.effective_chat.id)).format(
+            html.escape(chat.title), mention_html(user.id, user.first_name))  # PINNED_HTML
 
     return ""
 
@@ -209,7 +221,8 @@ def unpin(update: Update, context: CallbackContext) -> str:
             raise
 
     return get_string("admin", "UNPINNED_HTML", lang.get_lang(update.effective_chat.id)).format(html.escape(chat.title),
-                                       mention_html(user.id, user.first_name)) # UNPINNED_HTML
+                                                                                                mention_html(user.id,
+                                                                                                             user.first_name))  # UNPINNED_HTML
 
 
 @run_async
@@ -228,16 +241,20 @@ def invite(update: Update, context: CallbackContext):
             invitelink = bot.exportChatInviteLink(chat.id)
             update.effective_message.reply_text(invitelink)
         else:
-            update.effective_message.reply_text(get_string("admin", "ERR_NO_PERMS_INVITELINK", lang.get_lang(update.effective_chat.id))) # ERR_NO_PERMS_INVITELINK
+            update.effective_message.reply_text(get_string("admin", "ERR_NO_PERMS_INVITELINK", lang.get_lang(
+                update.effective_chat.id)))  # ERR_NO_PERMS_INVITELINK
     else:
-        update.effective_message.reply_text(get_string("admin", "ERR_NO_PERMS_INVITELINK", lang.get_lang(update.effective_chat.id))) # ERR_NO_SUPERGROUP
+        update.effective_message.reply_text(get_string("admin", "ERR_NO_PERMS_INVITELINK",
+                                                       lang.get_lang(update.effective_chat.id)))  # ERR_NO_SUPERGROUP
 
 
 @run_async
 @typing_action
 def adminlist(update: Update, context: CallbackContext):
     administrators = update.effective_chat.get_administrators()
-    text = get_string("admin", "ADMINS_IN", lang.get_lang(update.effective_chat.id)).format(update.effective_chat.title or get_string("admin", "THIS_CHAT", lang.get_lang(update.effective_chat.id))) # ADMINS_IN and THIS_CHAT
+    text = get_string("admin", "ADMINS_IN", lang.get_lang(update.effective_chat.id)).format(
+        update.effective_chat.title or get_string("admin", "THIS_CHAT",
+                                                  lang.get_lang(update.effective_chat.id)))  # ADMINS_IN and THIS_CHAT
     for admin in administrators:
         user = admin.user
         name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
@@ -250,15 +267,16 @@ def adminlist(update: Update, context: CallbackContext):
 
 def __chat_settings__(chat_id, user_id):
     return get_string("admin", "YOU_ADMIN", lang.get_lang(chat_id)).format(
-        dispatcher.bot.get_chat_member(chat_id, user_id).status in ("administrator", "creator")) # YOU_ADMIN
+        dispatcher.bot.get_chat_member(chat_id, user_id).status in ("administrator", "creator"))  # YOU_ADMIN
 
 
 def __help__(update: Update) -> str:
     return get_string("admin", "HELP", lang.get_lang(update.effective_chat.id))
 
+
 # HELP
 
-__mod_name__ = "Admin" # MODULE_NAME
+__mod_name__ = "Admin"  # MODULE_NAME
 
 PIN_HANDLER = CommandHandler("pin", pin, pass_args=True, filters=Filters.group)
 UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
@@ -270,11 +288,11 @@ DEMOTE_HANDLER = CommandHandler("demote", demote, pass_args=True, filters=Filter
 
 ADMINLIST_HANDLER = CommandHandler("adminlist", adminlist, filters=Filters.group)
 
-MUTE_HANDLER = CommandHandler("globalmute", toggle_mute, filters=Filters.group)
-DELETE_HANDLER = MessageHandler(Filters.group, on_message)
+MUTE_HANDLER = CommandHandler("globalmute", toggle_mute)
+DELETE_HANDLER = MessageHandler(Filters.all & Filters.group, on_message)
 
 dispatcher.add_handler(MUTE_HANDLER)
-dispatcher.add_handler(DELETE_HANDLER)
+dispatcher.add_handler(DELETE_HANDLER, 70)  # I have no idea why this number is necessary, but without it it breaks...
 dispatcher.add_handler(PIN_HANDLER)
 dispatcher.add_handler(UNPIN_HANDLER)
 dispatcher.add_handler(INVITE_HANDLER)
